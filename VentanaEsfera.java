6970c1e61bbcc6f9bf816cc0816599673a15b72b
Jhony Alpala <jhoniher95@gmail.com>
2023-11-15 23:12:22 -0500

    Finalizada clase VentanaEsfera

--- VentanaEsfera.java
+++ VentanaEsfera.java
@@ -27,6 +27,40 @@ public class VentanaEsfera extends JFrame implements ActionListener {
         campoRadio=new JTextField();
         campoRadio.setBounds(100,20,135,23);
 
-        calcular
+        calcular=new JButton();
+        calcular.setText("Calcular");
+        calcular.setBounds(100,50,135,23);
+        calcular.addActionListener(this);
+
+        volumen=new JLabel();
+        volumen.setText("Volumen (cm3):");
+        volumen.setBounds(20,90,135,23);
+
+        superficie=new JLabel();
+        superficie.setText("Superficie (cm2):");
+        superficie.setBounds(20,120,135,23);
+
+        contenedor.add(radio);
+        contenedor.add(campoRadio);
+        contenedor.add(calcular);
+        contenedor.add(volumen);
+        contenedor.add(superficie);
+    }
+    public void actionPerformed(ActionEvent evento){
+        if(evento.getSource()==calcular){
+            boolean error=false;
+            try{
+                double radio=Double.parseDouble(campoRadio.getText());
+                Esfera esfera=new Esfera(radio);
+                volumen.setText("Volumen (cm3):"+String.format("%.2f",esfera.calcularVolumen()));
+                superficie.setText("Superficie (cm2):"+String.format("%.2f",esfera.calcularSuperficie()));
+            }catch (Exception e){
+                error=true;
+            }finally {
+                if(error){
+                    JOptionPane.showMessageDialog(null,"Campo nulo o error en formato de numero","Error",JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        }
     }
 }
